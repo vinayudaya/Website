6f522850c2df8dc198f03ae847b5106bc6320775
vinayudaya <vinayudaya0509@gmail.com>
2024-03-09 21:16:25 +0530

    minify

--- include.js
+++ include.js
@@ -1,35 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Include the header
-  const headerContainer = document.getElementById("header-container");
-  const headerRequest = new XMLHttpRequest();
-  headerRequest.open("GET", "header.html", true);
-  headerRequest.onload = function () {
-    if (headerRequest.status >= 200 && headerRequest.status < 400) {
-      headerContainer.innerHTML = headerRequest.responseText;
-
-      // Now that header content is loaded, access elements within it
-      const themeIcon = document.getElementById("themeIcon");
-      if (themeIcon) {
-        const themeIcon = document.getElementById("themeIcon");
-        if (localStorage.getItem("theme") === "dark") {
-          themeIcon.src = "dark-icon.svg";
-        } else if (localStorage.getItem("theme") === "light") {
-          themeIcon.src = "light-icon.svg";
+  const e = document.getElementById("header-container"),
+    t = new XMLHttpRequest();
+  t.open("GET", "header.html", !0),
+    (t.onload = function () {
+      if (t.status >= 200 && t.status < 400) {
+        e.innerHTML = t.responseText;
+        if (document.getElementById("themeIcon")) {
+          const e = document.getElementById("themeIcon");
+          "dark" === localStorage.getItem("theme")
+            ? (e.src = "dark-icon.svg")
+            : "light" === localStorage.getItem("theme") &&
+              (e.src = "light-icon.svg");
         }
       }
-    }
-  };
-  headerRequest.send();
-
-  // Include the footer
-  const footerContainer = document.getElementById("footer-container");
-  const footerRequest = new XMLHttpRequest();
-  footerRequest.open("GET", "footer.html", true);
-  footerRequest.onload = function () {
-    if (footerRequest.status >= 200 && footerRequest.status < 400) {
-      footerContainer.innerHTML = footerRequest.responseText;
-      document.getElementById("year").innerHTML = new Date().getFullYear();
-    }
-  };
-  footerRequest.send();
+    }),
+    t.send();
+  const n = document.getElementById("footer-container"),
+    o = new XMLHttpRequest();
+  o.open("GET", "footer.html", !0),
+    (o.onload = function () {
+      o.status >= 200 &&
+        o.status < 400 &&
+        ((n.innerHTML = o.responseText),
+        (document.getElementById("year").innerHTML = new Date().getFullYear()));
+    }),
+    o.send();
 });
--- main.js
+++ main.js
@@ -1,28 +1,18 @@
-// main.js
 function toggleTheme() {
-  const body = document.body;
-  const themeIcon = document.getElementById("themeIcon");
-
-  // Toggle between light and dark themes
-  if (body.classList.contains("light")) {
-    body.classList.remove("light");
-    body.classList.add("dark");
-    themeIcon.src = "dark-icon.svg";
-    localStorage.setItem("theme", "dark");
-  } else {
-    body.classList.remove("dark");
-    body.classList.add("light");
-    themeIcon.src = "light-icon.svg";
-    localStorage.setItem("theme", "light");
-  }
+  const t = document.body,
+    e = document.getElementById("themeIcon");
+  t.classList.contains("light")
+    ? (t.classList.remove("light"),
+      t.classList.add("dark"),
+      (e.src = "dark-icon.svg"),
+      localStorage.setItem("theme", "dark"))
+    : (t.classList.remove("dark"),
+      t.classList.add("light"),
+      (e.src = "light-icon.svg"),
+      localStorage.setItem("theme", "light"));
 }
-
 document.addEventListener("DOMContentLoaded", function () {
-  // Load theme from localStorage
-  const storedTheme = localStorage.getItem("theme");
-  if (storedTheme === "dark") {
-    document.body.classList.add("dark");
-  } else {
-    document.body.classList.add("light");
-  }
+  "dark" === localStorage.getItem("theme")
+    ? document.body.classList.add("dark")
+    : document.body.classList.add("light");
 });
